db5a367bfebee7dc32138467b05d3412f1917855
Piotr Dziedzic <piotr.dziedzic1729@outlook.com>
2020-09-20 17:44:03 +0200

    6.7 extra exercise OperationsWithDataClass

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/OperationsWithData.java
@@ -0,0 +1,41 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.*;
+
+public class OperationsWithData {
+
+    int a;
+    int b;
+    int c;
+    double d;
+    double e;
+
+    private Statistics statistics;
+
+    public OperationsWithData(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics) {
+        int amountOfForumUsers = statistics.usersNames().size();
+        int amountOfPosts = statistics.postsCount();
+        int amountOfComments = statistics.commentsCount();
+        double averageAmountOfPostsPerUser = statistics.postsCount()/statistics.usersNames().size();
+        double averageAmountOfCommentsPerUser = statistics.commentsCount()/statistics.usersNames().size();
+        a = amountOfForumUsers;
+        b = amountOfPosts;
+        c = amountOfComments;
+        d = averageAmountOfPostsPerUser;
+        e = averageAmountOfCommentsPerUser;
+    }
+
+    public void showData() {
+        OperationsWithData xyz = new OperationsWithData(Statistics statistics);
+        xyz.calculateAdvStatistics(Statistics statistics);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
