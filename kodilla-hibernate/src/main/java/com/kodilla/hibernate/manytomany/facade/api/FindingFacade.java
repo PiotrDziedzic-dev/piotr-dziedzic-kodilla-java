a1a4d7a9f17a51f0a491e8f5c140f599c2975f4a
Piotr Dziedzic <piotr.dziedzic1729@outlook.com>
2021-03-17 03:00:12 +0100

    30.3 task

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/api/FindingFacade.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/api/FindingFacade.java
@@ -2,32 +2,36 @@ package com.kodilla.hibernate.manytomany.facade.api;
 
 import com.kodilla.hibernate.manytomany.Company;
 import com.kodilla.hibernate.manytomany.Employee;
+import com.kodilla.hibernate.manytomany.dao.CompanyDao;
+import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-
 import java.util.List;
+
 
 @Service
 public class FindingFacade {
 
-    private static final Logger LOGGER = new LoggerFactory.getLogger(FindingFacade.class);
-    private final FindingService findingService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(FindingFacade.class);
 
     @Autowired
-    public FindingFacade(FindingService findingService) {
-        this.findingService=findingService;
-    }
+    private EmployeeDao employeeDao;
 
-    public List<Company> findCompany(String company) {
-        LOGGER.info("Finding certain Company");
-        return findingService.findCompany(company);
+    @Autowired
+    private CompanyDao companyDao;
+
+    public List<Company> findCompany(String name) {
+        LOGGER.info("aaa");
+        return companyDao.retrieveCompaniesWitchHaveWord(name);
+
+
     }
     public List<Employee> findingEmployee(String name) {
-        LOGGER.info("Finding certain employee");
-        return findingService.findingEmployee(name);
+        LOGGER.info("aaa");
+        return employeeDao.retrieveEmployeesWhichHaveWord(name);
+
     }
 
 }
